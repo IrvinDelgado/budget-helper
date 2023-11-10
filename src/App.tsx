d5c1fc427cd8132d4859dcfe4dc1954a4a77d3c9
Irvin Delgado-Ramirez <irvind21@gmail.com>
2023-11-09 20:56:39 -0600

    feat: use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import BudgetCalculator from "./components/BudgetCalculator/BudgetCalculator"
-import BudgetBreakdown from "./components/BudgetBreakdown/BudgetBreakdown"
+import BudgetCalculator from "./components/BudgetCalculator/BudgetCalculator";
+import BudgetBreakdown from "./components/BudgetBreakdown/BudgetBreakdown";
 
 const App = () => {
-	return (
-    <div className="bg-gradient-to-r from-slate-600 to-slate-800">
-			<div>Budget Helper</div>
-			<BudgetCalculator/>
-			<BudgetBreakdown/>
+  return (
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      <div>Budget Helper</div>
+      <BudgetCalculator />
+      <BudgetBreakdown />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
