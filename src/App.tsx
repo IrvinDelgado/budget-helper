83c58287a735416db4645174f0d0571ae16eed4a
Irvin Delgado-Ramirez <irvind21@gmail.com>
2023-11-10 23:54:11 -0600

    feat: budget calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import BudgetCalculator from "./components/BudgetCalculator/BudgetCalculator";
 // import BudgetBreakdown from "./components/BudgetBreakdown/BudgetBreakdown";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-gradient-to-r from-slate-600 to-slate-800">
-      <h1 className="p-4 text-4xl font-bold text-white">Budget Helper</h1>
-      <BudgetCalculator />
-      {/* <BudgetBreakdown /> */}
-    </div>
+    <MantineProvider>
+      <div className="h-screen w-screen bg-gradient-to-r from-slate-600 to-slate-800">
+        <h1 className="p-4 text-4xl font-bold text-white">Budget Helper</h1>
+        <BudgetCalculator />
+      </div>
+    </MantineProvider>
   );
 };
 
--- src/components/BudgetCalculator/BudgetCalculator.tsx
+++ src/components/BudgetCalculator/BudgetCalculator.tsx
@@ -1,5 +1,58 @@
+import { NumberFormatter, NumberInput } from "@mantine/core";
+import { useState } from "react";
+type BudgetNumberType = {
+  value: number | string;
+};
+const BudgetNumber = ({ value }: BudgetNumberType) => {
+  return (
+    <NumberFormatter
+      className={"text-2xl font-bold text-green-700"}
+      prefix="$"
+      value={value}
+      thousandSeparator
+      fixedDecimalScale
+      decimalScale={2}
+    />
+  );
+};
+
 const BudgetCalculator = () => {
-  return <div className="m-4 w-1/3 bg-slate-300 p-4"></div>;
+  const [monthlyIncome, setMonthlyIncome] = useState<string | number>("");
+
+  return (
+    <div className="m-4 w-1/2 rounded-lg bg-stone-200 p-4">
+      <div className="flex justify-center">
+        <NumberInput
+          classNames={{
+            root: "w-3/4 pb-4",
+            label: "font-bold",
+            input: "bg-slate-500 text-center text-black font-bold bg-stone-300",
+          }}
+          label="Monthly Income"
+          placeholder="$0.00"
+          hideControls
+          thousandSeparator=","
+          prefix="$"
+          decimalScale={2}
+          fixedDecimalScale
+          value={monthlyIncome}
+          onChange={setMonthlyIncome}
+        />
+      </div>
+      <div>
+        <div className="flex justify-around pb-8">
+          <span>Needs</span>
+          <span>Wants</span>
+          <span>Save</span>
+        </div>
+        <div className="flex justify-between pb-8">
+          <BudgetNumber value={+monthlyIncome * 0.5} />
+          <BudgetNumber value={+monthlyIncome * 0.2} />
+          <BudgetNumber value={+monthlyIncome * 0.3} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BudgetCalculator;
